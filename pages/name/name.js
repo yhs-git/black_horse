9e55db626662f84d30ff522eb878be8cf7ed9414
云浮水 <8137735+cloud-floating-water@user.noreply.gitee.com>
2021-04-07 21:41:19 +0800

    Initial Commit

---
+++ pages/name/name.js
@@ -0,0 +1,75 @@
+// pages/name/name.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:'hello world',
+    src:'/static/yyqx.jpeg',
+    userInfo:''
+  },
+  getMyInfor:function (e) {
+    console.log('吃饭了')
+    console.log(e);
+    let info=e.detail.userInfo;
+    this.setData({
+     
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
